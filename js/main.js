913e5653167fe053f9542050d9a8956badd97911
Eran Sevil <57147896+eransevil@users.noreply.github.com>
2021-02-07 17:07:46 +0200

    adding  Hamburger button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-function  mouseOver(num){
-    document.querySelector(`.arrow${num}`).style.display = "inline-block";
-
+function mouseOver(num) {
+  document.querySelector(`.arrow${num}`).style.display = 'inline-block';
 }
 
 function mouseOut(num) {
-    document.querySelector(`.arrow${num}`).style.display = "none";
-  }
+  document.querySelector(`.arrow${num}`).style.display = 'none';
+}
+
+function toggleMenu() {
+  document.body.classList.toggle('menu-open');
+  
+}
